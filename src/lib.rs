c510e0835e848026df1366842e68151bb0d8525a
Patrick Chan <patrickchan1999@gmail.com>
2018-07-23 19:45:51 -0400

    Add function for retrieving OS and a placeholder for saving keybinding config

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,42 @@ extern crate sciter;
 
 mod load_handler;
 
+use sciter::Value;
+
 struct EventHandler;
 
 impl EventHandler {
+
+    fn get_os(&self) -> String {
+        if cfg!(target_os = "windows") {
+            String::from("Windows")
+        } else if cfg!(target_os = "macos") {
+            String::from("Mac OS")
+        } else if cfg!(target_os = "linux") {
+            String::from("Linux")
+        } else {
+            String::from("Other")
+        }
+    }
+
+    fn save_keybindings_config(&self, config: sciter::Value) -> bool {
+        println!("{}", config.get_item("show-window"));
+        println!("{}", config.get_item("hide-window"));
+        println!("{}", config.get_item("previous-option"));
+        println!("{}", config.get_item("next-option"));
+        println!("{}", config.get_item("execute-primary-action"));
+        println!("{}", config.get_item("execute-secondary-action"));
+        true
+    }
 
 }
 
 impl sciter::EventHandler for EventHandler {
 
+
     dispatch_script_call! {
+        fn get_os();
+        fn save_keybindings_config(Value);
     }
 
 }
